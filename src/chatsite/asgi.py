a00c48e72b505101be34c340c66af333fc3ba5ce
Iain04 <iain.lim.tao.en@gmail.com>
2023-08-30 17:39:57 +0800

    Update asgi.py and added static file consumer

--- src/chatsite/asgi.py
+++ src/chatsite/asgi.py
@@ -14,6 +14,8 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
+from channels.staticfiles import StaticFilesConsumer 
+
 import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatsite.settings')
@@ -27,5 +29,6 @@ application = ProtocolTypeRouter(
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
         ),
+        "static": StaticFilesConsumer,  # Include the StaticFilesConsumer
     }
 )
